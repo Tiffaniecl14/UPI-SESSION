512c9769b6a7db60e4c91f4bd31321ca0e94889f
Tiffaniecl14 <43336031+Tiffaniecl14@users.noreply.github.com>
2018-09-17 11:37:58 +0200

    Ajout d'un premier fichier

---
+++ calculvalexact.py
@@ -0,0 +1,11 @@
+#Tiffanie CARLIER
+#-*- coding: utf-8 -*-
+
+#solution exacte pour la question 2
+
+#calcul de la constante en fonction de la condition initiale
+def calculvalexact(t0,yo,t):
+    C=((1/(y0-1))-(1/2)*exp(2*t0))/exp(-2*t0)
+    yex=1+(1/(c*exp(-2*t))+(1/2)*exp(2*t)
+    return [yex]
+    
